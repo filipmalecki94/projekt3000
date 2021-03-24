ab4660e55a1533f71b3c99d8d57be9d3b453d97f
fifi <filipmalecki94@gmail.com>
2021-03-24 21:54:29 +0100

    backup commit 24_03_2021

--- js/index.js
+++ js/index.js
@@ -2,8 +2,7 @@ var maxValue = 10,
     collectionSize = 5;
 
 $(document).ready(function () {
-    $(location.hash.toString()).addClass('current');
-    initInsertSort();
+    changeSortPage();
 });
 
 $(window).resize(function (){
@@ -13,19 +12,45 @@ $(window).resize(function (){
 window.onhashchange = function () {
    $('.main-menu a').removeClass('current');
    $(location.hash.toString()).addClass('current');
+   changeSortPage();
 };
+
+function changeSortPage() {
+    var hash = location.hash.toString();
+
+    $(hash).addClass('current');
+    $('.code-block').empty();
+    $('.graph').empty();
+    $('.next').unbind();
+    switch (hash){
+        case '#insertion-sort':
+            initInsertSort();
+            break;
+        case '#counting-sort':
+            break;
+        case '#quicksort':
+            initQuicksort();
+            break;
+        default:
+            console.log('x')
+            break
+    }
+}
 
 function initInsertSort() {
     initCollection(true);
     initInsertionSortCode();
 }
 
+function initQuicksort() {
+    initCollection();
+    initQuicksortCode();
+}
+
 function initInsertionSortCode() {
-    var $codeField = $('<div/>',{
-                        'class': 'code m-1'
-                    }),
+    var $codeField = $('<div/>',{'class': 'code m-1'}),
         codeStructure = [
-        {'line' : 'int i,key,j;','tab':0},
+        {'line' : 'int <span style="color: red">i</span>,<span style="color: green">key</span>,<span style="color: blue">j</span>;','tab':0},
         {'line' : 'for(i = 1; i < n; i++) {', 'tab' : 0},
         {'line' : 'key = arr[i];', 'tab' : 1},
         {'line' : 'j = i - 1;', 'tab' : 1},
@@ -37,19 +62,38 @@ function initInsertionSortCode() {
         {'line' : '}', 'tab' : 0},
     ];
 
-    $.each(codeStructure,function (index,value){
-        console.log(value.line)
-        $codeField.append(
-            $('<div/>',{
-                'class' : 'step ' + index,
-                'text' : value.line
-            }).css({
-                'margin-left' : value.tab * 10
-            })
-        )
-    });
-
+    initCode(codeStructure,$codeField);
     $('.code-block').append($codeField);
+}
+
+function initQuicksortCode() {
+    var $codeFieldSort = $('<div/>',{'class': 'code m-1 h-50'}),
+        $codeFieldPartition = $('<div/>',{'class': 'code m-1 h-50'}),
+        codeStructureSort = [
+            {'line' : 'if(low < high) {', 'tab' : 0},
+            {'line' : 'int pi = partition(arr, low, high);', 'tab' : 1},
+            {'line' : '&nbsp;', 'tab' : 1},
+            {'line' : 'quickSort(arr, pi + 1, high);', 'tab' : 1},
+            {'line' : 'quickSort(arr, low, pi - 1);', 'tab' : 1},
+            {'line' : '}', 'tab' : 0},
+        ],
+        codeStructurePartition = [
+            {'line' : 'int pivot = arr[high];', 'tab' : 0},
+            {'line' : 'int i = (low - 1);', 'tab' : 0},
+            {'line' : '&nbsp;', 'tab' : 0},
+            {'line' : 'for(int j = low; j <= high-1; j++) {', 'tab' : 0},
+            {'line' : 'if(arr[j] < pivot) {', 'tab' : 1},
+            {'line' : 'i++;', 'tab' : 2},
+            {'line' : 'swap(&arr[i}, &arr[j]);', 'tab' : 2},
+            {'line' : '}', 'tab' : 1},
+            {'line' : '}', 'tab' : 0},
+            {'line' : 'swap(&arr[i+1},&arr[high];', 'tab' : 0},
+            {'line' : 'return (i+1);', 'tab' : 0},
+        ];
+
+        initCode(codeStructureSort,$codeFieldSort);
+        initCode(codeStructurePartition,$codeFieldPartition);
+        $('.code-block').append($codeFieldPartition).append($codeFieldSort)
 }
 
 function initCollection ($withNull = false) {
@@ -69,6 +113,20 @@ function getCollectionArr() {
     }
 
     return collection;
+}
+
+function initCode(codeStructure,$codeField) {
+    $.each(codeStructure,function (index,value){
+        $codeField.append(
+            $('<div/>',{
+                'class' : 'step ' + index,
+                'html' : value.line
+            }).css({
+                'margin-left' : value.tab * 10
+            })
+        )
+    });
+
 }
 
 function createBar (value) {
